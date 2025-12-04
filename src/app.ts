41f1d61cb6ef9ee3ec70881f1e906e835ba16a92
neamatullah01 <neamat22205101908@diu.edu.bd>
2025-12-05 01:51:13 +0600

    db setup and all tables created

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from "express";
+import initDB from "./config/db";
 
 const app = express();
+app.use(express.json());
+
+initDB();
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Vehicle server is running...");
---
+++ src/config/db.ts
@@ -0,0 +1,44 @@
+import { Pool } from "pg";
+import config from ".";
+
+const pool = new Pool({
+  connectionString: config.connection_str,
+});
+
+const initDB = async () => {
+  await pool.query(`
+        CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(250) NOT NULL,
+        email VARCHAR(250) NOT NULL UNIQUE CHECK (email = LOWER(email)),
+        password VARCHAR(250) NOT NULL CHECK (LENGTH(password) >= 6),
+        phone VARCHAR(20) NOT NULL,
+        role VARCHAR(20) NOT NULL CHECK (role IN ('admin', 'customer'))
+        )
+        `);
+
+  await pool.query(`
+        CREATE TABLE IF NOT EXISTS vehicles (
+        id SERIAL PRIMARY KEY,
+        vehicle_name VARCHAR(250) NOT NULL,
+        type VARCHAR(20) CHECK (type IN ('car', 'bike', 'van', 'SUV')),
+        registration_number VARCHAR(50) NOT NULL UNIQUE,
+        daily_rent_price NUMERIC(10,2) NOT NULL CHECK (daily_rent_price > 0),
+        availability_status VARCHAR(20) CHECK (availability_status IN ('available', 'booked'))
+        )
+        `);
+
+  await pool.query(`
+        CREATE TABLE IF NOT EXISTS bookings(
+        id SERIAL PRIMARY KEY,
+        customer_id INT NOT NULL REFERENCES users(id),
+        vehicle_id INT NOT NULL REFERENCES vehicles(id),
+        rent_start_date DATE NOT NULL,
+        rent_end_date DATE NOT NULL CHECK (rent_end_date >= rent_start_date),
+        total_price NUMERIC(10,2) NOT NULL CHECK (total_price > 0),
+        status VARCHAR(20) CHECK (status IN ('active', 'cancelled', 'returned'))
+        )
+        `);
+};
+
+export default initDB;
